c361dacfde30d06fce4a64ebcdf5273d2d8a2058
Philipp Stucki <philipp@qwert.ch>
2012-03-09 20:45:18 +0100

    fetch original community cookbooks

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
- 
+
 # Usage: ./deploy.sh [host]
- 
+
+
+# update community cookbooks to the latest
+pushd . > /dev/null
+if [ ! -e "cookbooks" ]
+then
+    git clone git://github.com/opscode/cookbooks.git cookbooks
+fi
+
+cd cookbooks && git fetch origin && git reset --hard origin/HEAD
+popd > /dev/null
+
 host="${1:-aaron@192.168.2.105}"
- 
+
 # The host key might change when we instantiate a new VM, so
 # we remove (-R) the old host key from known_hosts
 ssh-keygen -R "${host#*@}" 2> /dev/null
- 
+
 tar cj . | ssh -o 'StrictHostKeyChecking no' "$host" '
 sudo rm -rf ~/chef &&
 mkdir ~/chef &&
